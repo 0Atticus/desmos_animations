e254fd274bef0c0fd158a940bc89f5cd6d41224e
0Atticus <personman3personman3@gmail.com>
2022-12-30 20:15:27 -0500

    COLORS UPDATE

--- desmos.py
+++ desmos.py
@@ -5,6 +5,7 @@ import potrace
 import time
 from flask import Flask, render_template
 from os import sys, system
+import math
 
 app = Flask(__name__)
 
@@ -37,6 +38,7 @@ def get_latex(filename):
             start = segment.end_point
     return latex
 
+
 def get_contours(filename):
         
     im = cv2.imread(filename)
@@ -49,7 +51,19 @@ def get_contours(filename):
     cv2.imwrite(f"{filename.split('.')[0]}.pnm", mask)
     return (f"{im.shape[1]},{im.shape[0]}")
     
+def rgb_to_hex(rgb):
+    return '%02x%02x%02x' % rgb
 
+def get_points(filename):
+    colors = []
+    points = ""
+    img = Image.open(filename)
+    for y in range(img.height):
+        for x in range(img.width):
+            if (x % 10 == 0 and y % 10 == 0):
+                points += f"(-{x}, -{y})~"
+                colors.append(rgb_to_hex(img.getpixel((x, y))))
+    return (points[:-1], colors)
 
 def flip_image(filename):
     img = Image.open(filename)
@@ -57,16 +71,65 @@ def flip_image(filename):
     hzimg.save(filename)
 
 
+def seperate_colors(filename):
+    base_img = Image.open(filename)
+    width = base_img.size[0]
+    height = base_img.size[1]
+
+    init_image = np.zeros((height, width, 3), dtype=np.uint8)
+    init_image.fill(255)
+
+    new_color = (255, 255, 255)
+
+    for y in range(height):
+        new_color = (255, 255, 255)
+        for x in range(width):
+            color = base_img.getpixel((x, y))
+            r1, g1, b1 = color
+            if(x > 0):
+                prev_pix = base_img.getpixel((x-1, y))
+                
+
+                r2, g2, b2 = prev_pix
+                d = round(math.sqrt(0.3*(r1-r2)**2 + 0.59*(g1-g2)**2 + 0.11*(b1-b2)**2))
+                
+                if d > 55:
+                    if new_color == (255, 255, 255):
+                        new_color = (0, 0, 0)
+                    else:
+                        new_color = (255, 255, 255)
+
+            init_image[y, x] = new_color
+
+    data = Image.fromarray(init_image)
+    data.save("piss.png")
+
+
 @app.route("/")
 def plot_image():
+
+    seperate_colors(filename)
+    system(f"mv {filename} {filename.split('.')[0]}_color.png")
+    system(f"mv piss.png {filename}")
+
     flip_image(filename)
     shape = get_contours(filename)
     raw_latex = get_latex(filename.split(".")[0] + ".pnm")
     latex = ""
     flip_image(filename)
+
+    colored = filename.split(".")[0] + "_color.png"
+    flip_image(colored)
+    points, c = get_points(colored)
+    flip_image(colored)
+
+    colors = ""
+    for i in c:
+        colors += str(i) + "~"
+
     for bezier in raw_latex:
         latex += bezier
-    return render_template("index.html", latex=latex, shape=shape)
+    return render_template("index.html", latex=latex, shape=shape, colors=colors, points=points)
 
 
 if __name__ == "__main__":
